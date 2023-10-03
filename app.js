c45790b62a40cd58d9110850d46511cbb17a60eb
varsha766 <varshakumari370@gmail.com>
2023-10-03 13:23:39 +0530

    used openid-client

--- app.js
+++ app.js
@@ -4,61 +4,58 @@ import fetch from "node-fetch"
 import env from "dotenv"
 import cors from "cors"
 import http from 'http'
+import { Issuer, generators } from 'openid-client'
 const __dirname = path.resolve();
-
 env.config()
 const app = express();
 const server = http.createServer(app)
 const port = process.env.PORT
 app.use(express.static('public'))
 app.use(express.json())
-app.use(cors()
-)
-app.get('/', function (req, res) {
-    try {
-        res.sendFile("./public/login.html", { root: __dirname })
+app.use(cors())
 
-    } catch (e) {
-        res.send(`Error: ${e}`)
-    }
+
+// creating google issuer and client
+
+const googleIssuers = await Issuer.discover('https://accounts.google.com')
+const client = new googleIssuers.Client({
+    client_id: process.env.CLIENTID,
+    client_secret: process.env.CLIENT_SECRET,
+    redirect_uris: ["http://localhost:5000/cb"],
+    response_types: ['code']
 })
-app.get('/cb', async function (req, res) {
+const code_verifier = generators.codeVerifier()
+const code_challenge = generators.codeChallenge(code_verifier);
+async function fetchAuthUrlData() {
     try {
-
-        res.sendFile("./public/redirect.html", { root: __dirname })
-
-    } catch (e) {
-        res.send(`Error: ${e}`)
-    }
-})
-app.get('/accessToken', async function (req, res) {
-    try {
-        const { code } = req.query
-        if (!code) {
-            throw new Error('Code is mandatory')
-        }
-        const reqBody = {
-            code,
-            client_id: process.env.CLIENTID,
-            client_secret: process.env.CLIENT_SECRET,
-            redirect_uri: "http://localhost:5000/cb",
-            grant_type: "authorization_code"
-        }
-        const url = "https://oauth2.googleapis.com/token"
-        const response = await fetch(url, {
-            method: "POST",
-            body: new URLSearchParams(reqBody),
-            headers: {
-                "Content-Type": "application/x-www-form-urlencoded"
-            }
+        const authorizationUrl = client.authorizationUrl({
+            scope: 'openid email profile https://www.googleapis.com/auth/calendar.readonly https://www.googleapis.com/auth/calendar',
+            resource: 'https://www.googleapis.com/calendar/v3/users/me/calendarList',
+            code_challenge,
+            code_challenge_method: 'S256',
         })
-        const resp = await response.json()
-        res.json(resp)
+        console.log(authorizationUrl)
     } catch (e) {
         console.log(e)
+    }
+
+}
+fetchAuthUrlData()
+app.get('/cb', async function (req, res) {
+    try {
+        const params = client.callbackParams(req)
+        const tokenSet = await client.callback('http://localhost:5000/cb', params, { code_verifier })
+        console.log(tokenSet.TokenSet, "received tokenSet")
+        const accessToken = tokenSet.access_token
+        const userinfo = await client.userinfo(accessToken)
+        console.log(userinfo)
+        const clanderList = await client.requestResource("https://www.googleapis.com/calendar/v3/users/me/calendarList", accessToken)
+        console.log(clanderList)
+    } catch (e) {
         res.send(`Error: ${e}`)
     }
 })
+
 server.listen(port, () => {
     console.log('server is running on port: ', port)
 })
